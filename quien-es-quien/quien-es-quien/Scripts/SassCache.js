38280077d7cbcc71cd631911ef81bc8cee7e6b25
SrKomodo <komodosr@gmail.com>
2018-10-30 10:09:38 -0300

    Cool login view

---
+++ quien-es-quien/quien-es-quien/Scripts/SassCache.js
@@ -0,0 +1,21 @@
+﻿if (!localStorage.getItem("sass")) compile();
+else document.addEventListener("load", compile);
+
+async function compile() {
+  const sassElem = document.createElement("script");
+  sassElem.type = "text/javascript";
+  sassElem.src =
+    "https://cdnjs.cloudflare.com/ajax/libs/sass.js/0.10.11/sass.min.js";
+  sassElem.onload = () => {
+    const compiler = new Sass();
+    const compile = (text) => new Promise(function (resolve, reject) {
+      compiler.compile(text, result => resolve(result));
+    });
+    const css = "";
+    for (const link of document.querySelectorAll(`link[type="text/scss"]`)) {
+      css += await compile(await new Request(link.href).text());
+    }
+  };
+}
+
+function load() {}
