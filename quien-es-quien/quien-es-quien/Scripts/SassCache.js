59136d010734bab91a549f807a1565c8d01c3aec
SrKomodo <komodosr@gmail.com>
2018-11-01 09:06:39 -0300

    this breaks things but ok

--- quien-es-quien/quien-es-quien/Scripts/SassCache.js
+++ quien-es-quien/quien-es-quien/Scripts/SassCache.js
@@ -1,21 +1,34 @@
-﻿if (!localStorage.getItem("sass")) compile();
-else document.addEventListener("load", compile);
+﻿if (!localStorage.getItem("css")) compile();
+else document.onload = compile;
 
 async function compile() {
-  const sassElem = document.createElement("script");
-  sassElem.type = "text/javascript";
-  sassElem.src =
-    "https://cdnjs.cloudflare.com/ajax/libs/sass.js/0.10.11/sass.min.js";
-  sassElem.onload = () => {
-    const compiler = new Sass();
-    const compile = (text) => new Promise(function (resolve, reject) {
-      compiler.compile(text, result => resolve(result));
-    });
-    const css = "";
-    for (const link of document.querySelectorAll(`link[type="text/scss"]`)) {
-      css += await compile(await new Request(link.href).text());
-    }
-  };
+    console.log("hello");
+    const sassElem = document.createElement("script");
+    sassElem.type = "text/javascript";
+    sassElem.src =
+        "https://cdnjs.cloudflare.com/ajax/libs/sass.js/0.10.11/sass.min.js";
+    document.body.appendChild(sassElem);
+    sassElem.onload = async () => {
+        const compiler = new Sass("Scripts/sass.worker.js");
+        compiler.options({ style: Sass.style.compressed }, () => { });
+        const compile = (text) => new Promise(function (resolve, reject) {
+            compiler.compile(text, result => resolve(result));
+        });
+        let css = "";
+        for (const link of document.querySelectorAll(`link[type="text/scss"]`)) {
+            console.log("link", link.href);
+            const request = new Request(link.href);
+            const scss = await request.text();
+            console.log("css", scss);
+            css += JSON.stringify(await compile(scss));
+        }
+        localStorage.setItem("css", css);
+        load();
+    };
 }
 
-function load() {}
+function load() {
+    const el = document.createElement("style");
+    el.innerText = localStorage.getItem("css");
+    document.body.appendChild(el);
+}
