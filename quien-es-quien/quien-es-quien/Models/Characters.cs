038557d11b09b947190d30e6528ca00cf926b24a
Pedro P <pedroignacioperezvargas@gmail.com>
2018-11-15 08:55:21 -0300

    .

--- quien-es-quien/quien-es-quien/Models/Characters.cs
+++ quien-es-quien/quien-es-quien/Models/Characters.cs
@@ -5,6 +5,7 @@ using System.Web;
 
 namespace quien_es_quien.Models {
     public class Character {
+        [System.ComponentModel.DataAnnotations.Required]
         public string name;
         public int id;
     }
