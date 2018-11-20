1ee447027694b6969f739fce5bb5e202dc8ae1ed
PeronTheDuck <pedroignacioperezvargas@gmail.com>
2018-11-19 22:09:08 -0300

    Fuck this shit, i wanna eat

--- quien-es-quien/quien-es-quien/App_Start/RouteConfig.cs
+++ quien-es-quien/quien-es-quien/App_Start/RouteConfig.cs
@@ -1,17 +1,25 @@
 ﻿using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace quien_es_quien {
-    public class RouteConfig {
-        public static void RegisterRoutes(RouteCollection routes) {
+namespace quien_es_quien
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+            /*routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index" ,id="asd" }
+                defaults: new { controller = "Home", action = "Index" ,id="" }
+            );*/
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}",
+                defaults: new { controller = "Home", action = "Index" }
             );
-            
+
         }
     }
 }
--- quien-es-quien/quien-es-quien/Models/User.cs
+++ quien-es-quien/quien-es-quien/Models/User.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Data.SqlClient;
 
-namespace quien_es_quien.Models {
-    public class User {
+namespace quien_es_quien.Models
+{
+    public class User
+    {
         private long _bitcoins;
-        private string _username;
+        private readonly string _username;
         private int _score;
         private int _bestscore;
-        private bool _admin;
+        private readonly bool _admin;
+        private int _id;
 
-        public User(long bitcoins, string username, int score, int bestscore, bool admin) {
+        public User(long bitcoins, string username, int score, int bestscore, bool admin)
+        {
             _bitcoins = bitcoins;
             _username = username;
             _score = score;
@@ -20,15 +22,28 @@ namespace quien_es_quien.Models {
             _admin = admin;
         }
 
+        public User(long bitcoins, string username, int score, int bestscore, bool admin, int id)
+        {
+            _bitcoins = bitcoins;
+            _username = username;
+            _score = score;
+            _bestscore = bestscore;
+            _admin = admin;
+            _id = id;
+        }
+
         public long Bitcoins { get => _bitcoins; set => _bitcoins = value; }
-        public string Username { get => _username; }
+        public string Username => _username;
         public int Score { get => _score; set => _score = value; }
         public int Bestscore { get => _bestscore; set => _bestscore = value; }
-        public bool Admin { get => _admin; }
+        public bool Admin => _admin;
+        public int id { get => _id; set => _id = value; }
 
-        public bool UpdateBitcoins(long bitcoins) {
-            if(this.Bitcoins - bitcoins < 0 && this.Bitcoins < 0) {
-                this.Bitcoins = 0;
+        public bool UpdateBitcoins(long bitcoins)
+        {
+            if (Bitcoins - bitcoins < 0 && Bitcoins < 0)
+            {
+                Bitcoins = 0;
             }
 
             Bitcoins = Bitcoins + bitcoins;
@@ -42,16 +57,20 @@ namespace quien_es_quien.Models {
             command.Parameters.AddWithValue("@username", Username);
             command.Parameters.AddWithValue("@bitcoins", bitcoins);
 
-            try {
+            try
+            {
                 SqlDataReader reader = command.ExecuteReader();
                 return Convert.ToBoolean(reader["code"]);
-            } catch(Exception ex) {
+            }
+            catch (Exception ex)
+            {
                 Console.WriteLine("Caught exception: " + ex.Message + "\nWrong username?");
                 return false;
             }
         }
 
-        public static User LoginUser(string username, string password) {
+        public static User LoginUser(string username, string password)
+        {
             byte[] hash = Utils.CreateMD5(password);
 
             DaB dab = new DaB();
@@ -63,13 +82,16 @@ namespace quien_es_quien.Models {
             command.Parameters.AddWithValue("@username", username);
             command.Parameters.AddWithValue("@password", hash);
 
-            try {
+            try
+            {
                 SqlDataReader reader = command.ExecuteReader();
 
-                if(reader.Read()) {
+                if (reader.Read())
+                {
                     int code = Convert.ToInt32(reader["code"]);
-                    if(code == 1) {
-                        String uname = reader["username"].ToString();
+                    if (code == 1)
+                    {
+                        string uname = reader["username"].ToString();
                         long bitcoins = Convert.ToInt64(reader["bitcoins"]);
                         int bestscore = Convert.ToInt32(reader["bestscore"]);
                         bool admin = Convert.ToBoolean(reader["admin"]);
@@ -77,13 +99,16 @@ namespace quien_es_quien.Models {
                     }
                 }
                 return null;
-            } catch(Exception ex) {
+            }
+            catch (Exception ex)
+            {
                 Console.WriteLine("Caught exception: " + ex.Message + "\nWrong credentials?");
                 return null;
             }
         }
 
-        public static User RegisterUser(string username, string password, bool admin) {
+        public static User RegisterUser(string username, string password, bool admin)
+        {
             byte[] hash = Utils.CreateMD5(password);
 
             DaB dab = new DaB();
@@ -96,20 +121,63 @@ namespace quien_es_quien.Models {
             command.Parameters.AddWithValue("@password", hash);
             command.Parameters.AddWithValue("@admin", Convert.ToInt32(admin));
 
-            try {
+            try
+            {
                 SqlDataReader reader = command.ExecuteReader();
 
-                if(reader.Read()) {
+                if (reader.Read())
+                {
                     int code = Convert.ToInt32(reader["code"]);
-                    if(code == 1) {
+                    if (code == 1)
+                    {
                         return new User(1000000, username, 0, 0, admin);
                     }
                 }
                 return null;
-            } catch(Exception ex) {
+            }
+            catch (Exception ex)
+            {
                 Console.WriteLine("Caught exception: " + ex.Message);
                 return null;
             }
+        }
+
+        public static List<User> ListUsers()
+        {
+            List<User> users_list = new List<User>();
+            Models.DaB daB = new Models.DaB();
+
+            SqlConnection c = new DaB().Connect();
+            SqlCommand command = c.CreateCommand();
+            command.CommandText = "sp_GetUsers";
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                User one_user = new User((long)reader["bitcoins"], reader["username"].ToString(), 0, (int)reader["bestscore"], reader["admin"].ToString() == "1", (int)reader["id"]);
+                users_list.Add(one_user);
+            }
+
+            return users_list;
+
+        }
+        
+        public static User GetUser(int id)
+        {
+            Models.DaB daB = new Models.DaB();
+
+            SqlConnection c = new DaB().Connect();
+            SqlCommand command = c.CreateCommand();
+            command.CommandText = "sp_GetUser";
+            command.CommandType = System.Data.CommandType.StoredProcedure;//Never forget pls
+            command.Parameters.AddWithValue("id", id);
+            SqlDataReader reader = command.ExecuteReader();
+            if(reader.Read())
+            {
+                return new User((long)reader["bitcoins"], reader["username"].ToString(), 0, (int)reader["bestscore"], reader["admin"].ToString() == "1", (int)reader["id"]);
+            }
+            return null;
         }
     }
 }
