3f0e7a525ede4ded51de7d17b6655df90f5f3b17
SrKomodo <komodosr@gmail.com>
2018-11-29 08:37:04 -0300

    add game controller

---
+++ quien-es-quien/quien-es-quien/Controllers/GameController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace quien_es_quien.Controllers {
+    public class GameController : Controller {
+        public ActionResult Index() => View();
+    }
+}
