6b444147e67d65c7a196a360cec277fbfc81b8e5
SrKomodo <komodosr@gmail.com>
2018-12-05 18:29:52 -0300

    pedro pls

--- quien-es-quien/quien-es-quien/Models/Utils.cs
+++ quien-es-quien/quien-es-quien/Models/Utils.cs
@@ -25,8 +25,8 @@ namespace quien_es_quien.Models {
         }
 
         public static SqlConnection Connect() {
-            SqlConnection sql = new SqlConnection(@"Server=DESKTOP-FBKD222\SQLEXPRESS;Database=QEQC01;Trusted_Connection=True;");
-            //SqlConnection sql = new SqlConnection("Server=10.128.8.16;Database=QEQC01;Uid=QEQC01;Pwd=QEQC01");
+            // git stash push quien-es-quien\quien-es-quien\Models\Utils.cs
+            SqlConnection sql = new SqlConnection("Server=10.128.8.16;Database=QEQC01;Uid=QEQC01;Pwd=QEQC01");
             sql.Open();
             return sql;
         }
