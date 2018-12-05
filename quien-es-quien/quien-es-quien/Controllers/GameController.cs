bda613fb6a1cb17f97670d9af09ae3368058ec8e
PeronTheDuck <pedroignacioperezvargas@gmail.com>
2018-12-05 20:10:17 -0300

    Added redirects for winning and losing. Added red mark for questions already asked.

--- quien-es-quien/quien-es-quien/Controllers/GameController.cs
+++ quien-es-quien/quien-es-quien/Controllers/GameController.cs
@@ -1,7 +1,7 @@
 ﻿using quien_es_quien.Models;
 using System.Collections.Generic;
-using System.Web.Mvc;
 using System.Diagnostics;
+using System.Web.Mvc;
 
 namespace quien_es_quien.Controllers
 {
@@ -13,12 +13,22 @@ namespace quien_es_quien.Controllers
             Session["remainingCharacters"] = remainingCharacters;
             Session["questionsAsked"] = new List<int>();
 
+            if (Session["User"] == null)
+            {
+                Session["GuestBitcoins"] = 10000;//10 questions
+            }
+
+
             System.Random random = new System.Random();
             int i = random.Next();
             int charactersCount = remainingCharacters.Count;
-            while (i > charactersCount) i = random.Next();
+            while (i > charactersCount)
+            {
+                i = random.Next();
+            }
+
             Session["secretCharacter"] = remainingCharacters[i];
-            Debug.Print("Secret character: "+ remainingCharacters[i].Id);
+            Debug.Print("Secret character: " + remainingCharacters[i].Id);
 
             ViewBag.characters = Session["remainingCharacters"];
             ViewBag.characteristics = Characteristic.ListCharacteristics();
@@ -36,16 +46,17 @@ namespace quien_es_quien.Controllers
             if (Session["User"] != null)
             {
                 ((User)Session["User"]).UpdateBitcoins(-1000);
-                if(((User)Session["User"]).Bitcoins <= 0)
+                if (((User)Session["User"]).Bitcoins <= 0)
                 {
-                    //No more questions left.
+                    return RedirectToAction("Lose");
                 }
             }
             else
             {
                 Session["GuestBitcoins"] = ((int)Session["GuestBitcoins"]) - 1000;
-                if(((int)Session["GuestBitcoins"]) <= 0){
-                    //No more questions left.
+                if (((int)Session["GuestBitcoins"]) <= 0)
+                {
+                    return RedirectToAction("Lose");
                 }
             }
 
@@ -54,9 +65,9 @@ namespace quien_es_quien.Controllers
                 if (secretCharacter.Characteristics[i].Type == type)
                 {
                     bool hasIt = secretCharacter.Characteristics[i].Id == characteristic;
-                    foreach(Character character in remainingCharacters)
+                    foreach (Character character in remainingCharacters)
                     {
-                        if((character.Characteristics[i].Id==characteristic) == hasIt)
+                        if ((character.Characteristics[i].Id == characteristic) == hasIt)
                         {
                             newRemainingCharacters.Add(character);
                         }
@@ -65,7 +76,7 @@ namespace quien_es_quien.Controllers
                 }
             }
 
-            if(newRemainingCharacters.Count==1)
+            if (newRemainingCharacters.Count == 1)
             {
                 return RedirectToAction("Win");
             }
@@ -82,7 +93,18 @@ namespace quien_es_quien.Controllers
 
         public ActionResult Win()
         {
+            int bitcoinsRemaining;
+            if (Session["User"] != null)
+            {
+                bitcoinsRemaining = (int)((User)Session["User"]).Bitcoins;
+            }
+            else
+            {
+                bitcoinsRemaining = (int)Session["GuestBitcoins"];
 
+            }
+            ViewBag.Bitcoins = bitcoinsRemaining;
+            ViewBag.SecretCharacter = Session["secretCharacter"];
 
             return View();
         }
