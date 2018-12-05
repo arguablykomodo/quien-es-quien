63c95f49dc0f11fd38162a9c68d210c199d3a572
PeronTheDuck <pedroignacioperezvargas@gmail.com>
2018-12-05 20:55:10 -0300

    Added logic to handle guessing

--- quien-es-quien/quien-es-quien/Controllers/GameController.cs
+++ quien-es-quien/quien-es-quien/Controllers/GameController.cs
@@ -6,28 +6,30 @@ using System.Web.Mvc;
 namespace quien_es_quien.Controllers {
     public class GameController : Controller {
         public ActionResult Index() {
-            List<Character> remainingCharacters = Character.ListCharactersDeep();
-            Session["remainingCharacters"] = remainingCharacters;
-            Session["questionsAsked"] = new List<int>();
+            List<Character> RemainingCharacters = Character.ListCharactersDeep();
+            Session["RemainingCharacters"] = RemainingCharacters;
+            Session["QuestionsAsked"] = new List<int>();
 
             if (Session["User"] == null)
             {
                 Session["GuestBitcoins"] = 10000;//10 questions
+            } else {
+                Session["OriginalBitcoins"] = ((User)Session["User"]).Bitcoins;
             }
 
 
             System.Random random = new System.Random();
             int i = random.Next();
-            int charactersCount = remainingCharacters.Count;
+            int charactersCount = RemainingCharacters.Count;
             while (i > charactersCount)
             {
                 i = random.Next();
             }
 
-            Session["secretCharacter"] = remainingCharacters[i];
-            Debug.Print("Secret character: " + remainingCharacters[i].Id);
+            Session["SecretCharacter"] = RemainingCharacters[i];
+            Debug.Print("Secret character: " + RemainingCharacters[i].Id);
 
-            ViewBag.characters = Session["remainingCharacters"];
+            ViewBag.characters = Session["RemainingCharacters"];
             ViewBag.characteristics = Characteristic.ListCharacteristics();
             ViewBag.types = CharacteristicType.ListCharactersisticType();
 
@@ -35,9 +37,9 @@ namespace quien_es_quien.Controllers {
         }
 
         public ActionResult Play(int type = -1, int characteristic = -1) {
-            List<Character> remainingCharacters = Session["remainingCharacters"] as List<Character>;
+            List<Character> RemainingCharacters = Session["RemainingCharacters"] as List<Character>;
             List<Character> newRemainingCharacters = new List<Character>();
-            Character secretCharacter = Session["secretCharacter"] as Character;
+            Character SecretCharacter = Session["SecretCharacter"] as Character;
 
             if (Session["User"] != null) {
                 ((User)Session["User"]).UpdateBitcoins(-1000);
@@ -54,10 +56,10 @@ namespace quien_es_quien.Controllers {
                 }
             }
 
-            for (int i = 0; i < secretCharacter.Characteristics.Count; i++) {
-                if (secretCharacter.Characteristics[i].Type == type) {
-                    bool hasIt = secretCharacter.Characteristics[i].Id == characteristic;
-                    foreach (Character character in remainingCharacters) {
+            for (int i = 0; i < SecretCharacter.Characteristics.Count; i++) {
+                if (SecretCharacter.Characteristics[i].Type == type) {
+                    bool hasIt = SecretCharacter.Characteristics[i].Id == characteristic;
+                    foreach (Character character in RemainingCharacters) {
                         if ((character.Characteristics[i].Id == characteristic) == hasIt) {
                             newRemainingCharacters.Add(character);
                         }
@@ -71,32 +73,58 @@ namespace quien_es_quien.Controllers {
                 return RedirectToAction("Win");
             }
 
-            Session["remainingCharacters"] = newRemainingCharacters;
-            ((List<int>)Session["questionsAsked"]).Add(characteristic);
+            if(type!=-1&&characteristic!=-1)
+                Session["RemainingCharacters"] = newRemainingCharacters;
+            ((List<int>)Session["QuestionsAsked"]).Add(characteristic);
 
-            ViewBag.characters = newRemainingCharacters;
+            ViewBag.characters = Session["RemainingCharacters"];
             ViewBag.characteristics = Characteristic.ListCharacteristics();
             ViewBag.types = CharacteristicType.ListCharactersisticType();
 
             return View();
         }
 
-        public ActionResult Win()
+        public ActionResult Guess(int id)
         {
-            int bitcoinsRemaining;
-            if (Session["User"] != null)
-            {
-                bitcoinsRemaining = (int)((User)Session["User"]).Bitcoins;
-            }
-            else
-            {
-                bitcoinsRemaining = (int)Session["GuestBitcoins"];
+            Character SecretCharacter = Session["SecretCharacter"] as Character;
 
+            ((User)Session["User"]).UpdateBitcoins(-3000);
+
+            if(SecretCharacter.Id==id)
+            {
+                return RedirectToAction("Win",new { bonus=true });
+            } else {
+                return RedirectToAction("Play");
             }
-            ViewBag.Bitcoins = bitcoinsRemaining;
-            ViewBag.SecretCharacter = Session["secretCharacter"];
+
+        }
+
+        public ActionResult Win(bool bonus = false)
+        {
+            long OriginalBitcoins = (long)Session["OriginalBitcoins"];
+
+            long BitcoinsRemaining;
+            if (Session["User"] != null) {
+                BitcoinsRemaining = (long)((User)Session["User"]).Bitcoins;
+
+                double multiplier;
+                if (bonus)
+                    multiplier = 2;
+                else
+                    multiplier = 1.2;
+
+                ((User)Session["User"]).UpdateBitcoins((long)System.Math.Floor((OriginalBitcoins - BitcoinsRemaining) * multiplier));
+            } else {
+                BitcoinsRemaining = (long)Session["GuestBitcoins"];
+            }
+
+
+            ViewBag.SpentBitcoins = OriginalBitcoins - BitcoinsRemaining;
+            ViewBag.Bitcoins = BitcoinsRemaining;
+            ViewBag.SecretCharacter = Session["SecretCharacter"];
             
             return View();
         }
+
     }
 }
